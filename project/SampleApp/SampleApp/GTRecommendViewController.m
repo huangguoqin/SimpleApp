b6d05aec9121bfb48d254a72b7fe7f9441ff0ef7
hgq <996225956@qq.com>
2021-12-08 19:51:50 +0800

    p20-pageEnabled翻页效果实现

--- project/SampleApp/SampleApp/GTRecommendViewController.m
+++ project/SampleApp/SampleApp/GTRecommendViewController.m
@@ -34,6 +34,31 @@
     // set content size
     scrollView.contentSize = CGSizeMake(self.view.bounds.size.width * 5, self.view.bounds.size.height);
     
+    
+    
+    
+    // 声明一个颜色数组
+    NSArray *colorArray = @[
+        [UIColor redColor],
+        [UIColor blueColor],
+        [UIColor greenColor],
+        [UIColor grayColor],
+        [UIColor cyanColor]
+    ];
+    
+    // 创建5个uiview填充content
+    for (int i = 0; i < 5; ++i) {
+        [scrollView addSubview:({
+            UIView *view = [[UIView alloc] initWithFrame:CGRectMake(self.view.bounds.size.width * i, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+            // 设置背景色
+            view.backgroundColor = colorArray[i];
+            view;
+        })];
+    }
+    
+    // 启用pagingEnabled，翻页效果
+    scrollView.pagingEnabled = YES;
+    
     [self.view addSubview:scrollView];
 }
 

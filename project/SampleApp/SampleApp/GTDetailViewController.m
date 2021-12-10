d1f59c264118f1ab131bf922bc50b0e4998b327e
hgq <996225956@qq.com>
2021-12-10 20:18:32 +0800

    p31-WKWebView kvo观察者模式监听progress

--- project/SampleApp/SampleApp/GTDetailViewController.m
+++ project/SampleApp/SampleApp/GTDetailViewController.m
@@ -12,19 +12,36 @@
 @interface GTDetailViewController ()<WKNavigationDelegate>
 
 @property(nonatomic, strong, readwrite) WKWebView *webView;
+@property(nonatomic, strong, readwrite) UIProgressView *progressView;
 
 @end
 
 @implementation GTDetailViewController
 
+- (void) dealloc{
+    // 页面销毁时移除监听
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress" context:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    
     // Do any additional setup after loading the view.
     [self.view addSubview:({
         self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, self.view.frame.size.height - 88)];
         self.webView.navigationDelegate = self;
+            
+        [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+        
         self.webView;
+    })];
+    
+    // 进度条
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, 20)];
+        self.progressView;
     })];
     
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://weixin.wstmart.net"]]];
@@ -39,6 +56,18 @@
     decisionHandler(WKNavigationActionPolicyAllow);
 }
 
+/**
+ 
+ */
+- (void)observeValueForKeyPath:(nullable NSString *)keyPath ofObject:(nullable id)object change:(nullable NSDictionary<NSKeyValueChangeKey, id> *)change context:(nullable void *)context{
+//    NSLog(@"===== %@", @(self.webView.estimatedProgress));
+    if(self.webView.estimatedProgress==1.0f){
+        self.progressView.hidden = YES;
+    }else{
+        [self.progressView setProgress:self.webView.estimatedProgress];
+    }
+    
+}
 /**
  加载完成后
  */
